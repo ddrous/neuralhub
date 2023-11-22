dcdd2fd1eced8fea9731d9e4b7f2753164e83965
rdes <gb21553@bristol.ac.uk>
2023-11-22 18:24:09 +0000

    Newton is completely worthless

--- examples/lotka-voltera/03_neural_ode.py
+++ examples/lotka-voltera/03_neural_ode.py
@@ -13,6 +13,8 @@ print("\n############# Lotka-Volterra with Neural ODE #############\n")
 print("Available devices:", jax.devices())
 
 from jax import config
+##  Debug nans
+config.update("jax_debug_nans", True)
 
 import jax.numpy as jnp
 import jax.scipy as jsp
@@ -34,7 +36,8 @@ import time
 
 #%%
 
-SEED = 27
+# SEED = 27
+SEED = np.random.randint(0, 1000)
 
 ## Integrator hps
 integrator = rk4_integrator
@@ -59,7 +62,7 @@ def lotka_volterra(t, state, alpha, beta, delta, gamma):
     dy_dt = delta * x * y - gamma * y
     return [dx_dt, dy_dt]
 
-p = {"alpha": 1.5, "beta": 1.0, "gamma": 1.0, "delta": 3.0}
+p = {"alpha": 1.5, "beta": 1.0, "delta": 3.0, "gamma": 1.0}
 t_eval = np.linspace(0, 10, 1001)
 initial_state = [1.0, 1.0]
 
@@ -73,21 +76,33 @@ print("data shape (first two are superfluous):", data.shape)
 # %%
 
 class Processor(eqx.Module):
-    layers: list
+    # layers: list
+    physics: jnp.ndarray
 
     def __init__(self, in_size=2, out_size=2, key=None):
         keys = get_new_key(key, num=3)
-        self.layers = [eqx.nn.Linear(in_size, 10, key=keys[0]), jax.nn.tanh,
-        # self.layers = [eqx.nn.Linear(in_size+1, 50, key=keys[0]), jax.nn.tanh,
-                        # eqx.nn.Linear(10, 10, key=keys[1]), jax.nn.tanh,
-                        eqx.nn.Linear(10, out_size, key=keys[2]) ]
+        # self.layers = [eqx.nn.Linear(in_size, 10, key=keys[0]), jax.nn.tanh,
+        # # self.layers = [eqx.nn.Linear(in_size+1, 50, key=keys[0]), jax.nn.tanh,
+        #                 # eqx.nn.Linear(10, 10, key=keys[1]), jax.nn.tanh,
+        #                 eqx.nn.Linear(10, out_size, key=keys[2]) ]
+
+        # self.physics = jnp.array([1.5, 1.0, 3.0, 1.0])
+        self.physics = jnp.abs(jax.random.normal(keys[1], (4,)))
 
     def __call__(self, x, t):
-        # y = jnp.concatenate([jnp.broadcast_to(t, (1,)), x], axis=0)
-        y = x
-        for layer in self.layers:
-            y = layer(y)
-        return y
+
+        ## Physics contribution
+        dx0 = x[0]*self.physics[0] - x[0]*x[1]*self.physics[1]
+        dx1 = x[0]*x[1]*self.physics[2] - x[1]*self.physics[3]
+
+        # ## Neural Net contribution
+        # # y = jnp.concatenate([jnp.broadcast_to(t, (1,)), x], axis=0)
+        # y = x
+        # for layer in self.layers:
+        #     y = layer(y)
+        # # return y
+        return jnp.array([dx0, dx1])
+
 
 # %%
 
@@ -118,82 +133,86 @@ def l2_norm(X, X_hat):
 
 # %%
 
-def loss_fn(params, static, batch):
-    print('\nCompiling function "loss_fn" ...\n')
-    X, t = batch
+#### ==== Vanilla Gradient Descent optimisation ==== ####
 
-    X_hat = integrator(params, static, X[0, 0, :], t, 1.4e-8, 1.4e-8, jnp.inf, jnp.inf, 50, "checkpointed")
+# def loss_fn(params, static, batch):
+#     print('\nCompiling function "loss_fn" ...\n')
+#     X, t = batch
 
-    return l2_norm(X, X_hat)
-    # return 1e0*params_norm(params_env)
-    # return l2_norm(X, X_hat) + 1e-0*params_norm(params_env)
+#     X_hat = integrator(params, static, X[0, 0, :], t, 1.4e-8, 1.4e-8, jnp.inf, jnp.inf, 50, "checkpointed")
 
-
-@partial(jax.jit, static_argnums=(1))
-def train_step(params, static, batch, opt_state):
-    print('\nCompiling function "train_step" ...\n')
-
-    loss, grads = jax.value_and_grad(loss_fn)(params, static, batch)
-
-    updates, opt_state = opt.update(grads, opt_state)
-    params = optax.apply_updates(params, updates)
-
-    return params, opt_state, loss
+#     return l2_norm(X, X_hat)
+#     # return 1e0*params_norm(params_env)
+#     # return l2_norm(X, X_hat) + 1e-0*params_norm(params_env)
 
 
-total_steps = nb_epochs
+# @partial(jax.jit, static_argnums=(1))
+# def train_step(params, static, batch, opt_state):
+#     print('\nCompiling function "train_step" ...\n')
 
-# sched = optax.exponential_decay(init_lr, total_steps, decay_rate)
-# sched = optax.linear_schedule(init_lr, 0, total_steps, 0.25)
-sched = optax.piecewise_constant_schedule(init_value=init_lr,
-                                            boundaries_and_scales={int(total_steps*0.25):0.5, 
-                                                                    int(total_steps*0.5):0.2,
-                                                                    int(total_steps*0.75):0.5})
-fig, ax = plt.subplots(1, 1, figsize=(6, 3.5))
+#     loss, grads = jax.value_and_grad(loss_fn)(params, static, batch)
 
-start_time = time.time()
+#     updates, opt_state = opt.update(grads, opt_state)
+#     params = optax.apply_updates(params, updates)
+
+#     return params, opt_state, loss
 
 
-print(f"\n\n=== Beginning Training ... ===")
+# total_steps = nb_epochs
 
-opt = optax.adam(sched)
-opt_state = opt.init(params)
+# # sched = optax.exponential_decay(init_lr, total_steps, decay_rate)
+# # sched = optax.linear_schedule(init_lr, 0, total_steps, 0.25)
+# sched = optax.piecewise_constant_schedule(init_value=init_lr,
+#                                             boundaries_and_scales={int(total_steps*0.25):0.5, 
+#                                                                     int(total_steps*0.5):0.2,
+#                                                                     int(total_steps*0.75):0.5})
+# fig, ax = plt.subplots(1, 1, figsize=(6, 3.5))
 
-losses = []
-for epoch in range(nb_epochs):
+# start_time = time.time()
 
-    nb_batches = 0
-    loss_sum = 0.
-    for i in range(0, 1, batch_size):
-        batch = (data[0,i:i+batch_size,...], t_eval)
+
+# print(f"\n\n=== Beginning Training ... ===")
+
+# opt = optax.adam(sched)
+# opt_state = opt.init(params)
+
+# losses = []
+# for epoch in range(nb_epochs):
+
+#     nb_batches = 0
+#     loss_sum = 0.
+#     for i in range(0, 1, batch_size):
+#         batch = (data[0,i:i+batch_size,...], t_eval)
     
-        params, opt_state, loss = train_step(params, static, batch, opt_state)
+#         params, opt_state, loss = train_step(params, static, batch, opt_state)
 
-        loss_sum += loss
-        nb_batches += 1
+#         loss_sum += loss
+#         nb_batches += 1
 
-    loss_epoch = loss_sum/nb_batches
-    losses.append(loss_epoch)
+#     loss_epoch = loss_sum/nb_batches
+#     losses.append(loss_epoch)
 
-    if epoch%print_every==0 or epoch<=3 or epoch==nb_epochs-1:
-        print(f"    Epoch: {epoch:-5d}      Loss: {loss_epoch:.8f}", flush=True)
+#     if epoch%print_every==0 or epoch<=3 or epoch==nb_epochs-1:
+#         print(f"    Epoch: {epoch:-5d}      Loss: {loss_epoch:.8f}", flush=True)
 
 
-# ax = sbplot(losses, x_label='Epoch', y_label='L2', y_scale="log", title=f'Loss for environment {e}', ax=ax);
-ax = sbplot(losses, x_label='Epoch', y_label='L2', y_scale="log", title='Losses', ax=ax);
-plt.savefig(f"data/loss_simple.png", dpi=300, bbox_inches='tight')
-# plt.show()
-plt.legend()
-fig.canvas.draw()
-fig.canvas.flush_events()
+# # ax = sbplot(losses, x_label='Epoch', y_label='L2', y_scale="log", title=f'Loss for environment {e}', ax=ax);
+# ax = sbplot(losses, x_label='Epoch', y_label='L2', y_scale="log", title='Losses', ax=ax);
+# plt.savefig(f"data/loss_simple.png", dpi=300, bbox_inches='tight')
+# # plt.show()
+# plt.legend()
+# fig.canvas.draw()
+# fig.canvas.flush_events()
 
-wall_time = time.time() - start_time
-time_in_hmsecs = seconds_to_hours(wall_time)
-print("\nTotal training time: %d hours %d mins %d secs" %time_in_hmsecs)
+# wall_time = time.time() - start_time
+# time_in_hmsecs = seconds_to_hours(wall_time)
+# print("\nTotal GD training time: %d hours %d mins %d secs" %time_in_hmsecs)
 
 
 
 # %%
+
+# #### ==== BFGS optimisation ==== ####
 
 @partial(jax.jit, static_argnums=(1))
 def loss_fn_(params_flat, static, batch):
@@ -209,10 +228,16 @@ def loss_fn_(params_flat, static, batch):
 
 params_flat, params_shapes, tree_def = flatten_pytree(params)
 
+start_time = time.time()
 
-results = jsp.optimize.minimize(loss_fn_, params_flat, args=(static, (data[0,:,...], t_eval)), tol=None, method='BFGS', options={'maxiter': 1000})
-# print("Success: ", results.x.shape, results)
-print("Optimisation result: ", results.success, results.nit, results.fun)
+results = jsp.optimize.minimize(loss_fn_, params_flat, args=(static, (data[0,:,...], t_eval)), tol=1e-10, method='BFGS', options={'maxiter': 1000})
+
+wall_time = time.time() - start_time
+time_in_hmsecs = seconds_to_hours(wall_time)
+print("\nTotal BFGS training time: %d hours %d mins %d secs" %time_in_hmsecs)
+
+
+print("\nOptimisation result: success, nit, loss_val: ", results.success, results.nit, results.fun)
 
 params_flat = results.x
 params = unflatten_pytree(params_flat, params_shapes, tree_def)
@@ -220,6 +245,8 @@ params = unflatten_pytree(params_flat, params_shapes, tree_def)
 
 
 # %%
+
+# #### ==== Newton optimisation ==== ####
 
 # from typing import List, Tuple, Callable
 # args=(static, (data[0,:,...], t_eval))
@@ -250,7 +277,8 @@ params = unflatten_pytree(params_flat, params_shapes, tree_def)
 #         _, x, lr, errors, nit = carry
 #         gradient = gf(x, *args)
 #         hessian = hf(x, *args)
-#         x_new = x - lr*jnp.linalg.solve(hessian, gradient)
+#         # x_new = x - lr*jnp.linalg.solve(hessian, gradient)
+#         x_new = x - lr * jnp.linalg.inv(hessian)@gradient
 
 #         errors = errors.at[nit+1].set(jnp.linalg.norm(x_new - x))
 #         return x, x_new, lr*lr_decr, errors, nit+1
@@ -264,16 +292,24 @@ params = unflatten_pytree(params_flat, params_shapes, tree_def)
 
 #     _, x, lr, errors, nit = jax.lax.while_loop(cond_fn, body_fn, (x, x, lr, errors, nit))
 
-
-
-
 #     # return x, points, nit
-#     return x, None, nit
+#     return x, None, errors, nit
 
 # gf = jax.jacrev(loss_fn_, argnums=0)
 # hf = jax.jacrev(gf, argnums=0)
+# params_flat, params_shapes, tree_def = flatten_pytree(params)
 
-# params_flat, _, nit = newton(params_flat, loss_fn_, gf, hf, maxiter=200, lr=0.1, lr_decr=0.999, tol=0.001)
+# start_time = time.time()
+
+# params_flat, _, errors, nit = newton(params_flat, loss_fn_, gf, hf, tol=1e-10, maxiter=200, lr=1e-1, lr_decr=0.999)
+
+# wall_time = time.time() - start_time
+# time_in_hmsecs = seconds_to_hours(wall_time)
+# print("\nTotal Newton training time: %d hours %d mins %d secs" %time_in_hmsecs)
+
+# print("\nOptimisation result: nit, loss_val: ", nit, errors[nit-1])
+
+# params = unflatten_pytree(params_flat, params_shapes, tree_def)
 
 
 # %%
@@ -309,6 +345,9 @@ eqx.tree_serialise_leaves("data/model_02.eqx", model)
 # %% [markdown]
 
 # # Preliminary results
+# - GD is slow and poor
+# - BFGS is fast, but seems to stop too early saying Failure, while it looks like success. I believe if do BFGS in a loop, we might get absolutely great results
+# - Newton is weird, for now !!
 
 
 # # Conclusion
